f7b8747f5061b2d6b158c60b3cb1188970e884ee
marishanmugam <mageshg1402@gmail.com>
2022-07-26 10:51:30 +0530

    checkBox

---
+++ checkBox.js
@@ -0,0 +1,33 @@
+let selectAll = document.querySelector(".form-group.select-all input");
+let allCheckBox = document.querySelectorAll(".form-group:not(.select-all) input"
+);
+
+let chkArr = [];
+
+allCheckBox.forEach((item) => {
+  item.addEventListener("change", () => {
+    allCheckBox.forEach((e) => {
+      chkArr.push(e.checked);
+    });
+    if (chkArr.includes(false)) {
+      selectAll.checked = false;
+    } else {
+      selectAll.checked = true;
+    }
+    chkArr = [];
+  });
+});
+
+selectAll.addEventListener("change", function () {
+  if (this.checked) {
+    allCheckBox.forEach((i) => {
+      i.checked = true;
+    });
+  } 
+  else 
+  {
+    allCheckBox.forEach((i) => {
+      i.checked = false;
+    });
+  }
+});
